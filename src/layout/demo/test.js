ad5e7344eef7cc4c32b74b206762f345b6f0d498
HuangYong <jinlong_8023@163.com>
2016-11-16 23:45:34 +0800

    readme

--- src/layout/demo/test.js
+++ src/layout/demo/test.js
@@ -1,12 +1,15 @@
 $(function(){
-  var debug     = true,
-      udLayout  = new UDLayout($(".ud-container"), debug)
-                    .registerEvents({
-                      completed : function(){console.log(this)}
-                    }).init(0.1),
-      lrLayout  = new LRLayout($(".lr-container"), debug)
-                    .registerEvents({completed : function(){console.log(this)}})
-                    .init(0.2);
+  var debug     = true;
+
+  // 设置上下布局
+  var udLayout  = new UDLayout($(".ud-container"), debug);
+  udLayout.registerEvents({completed : function(){console.log(this)}}).init(0.1);
+
+  // 设置左右布局
+  var lrLayout  = new LRLayout($(".lr-container"), debug)
+  lrLayout.registerEvents({completed : function(){console.log(this)}}).init(0.2);
+
+  // 监控窗口变化
   $(".ud-container").height(document.documentElement.clientHeight);
   $(window).resize(function(){
     $(".ud-container").height(document.documentElement.clientHeight);
